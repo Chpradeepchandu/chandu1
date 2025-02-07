dba71dccaed2bd4b88c07c66d2715f11cad2c7a5
PRADEEP <pradeepchanduc@gmail.com>
2025-02-07 15:10:01 +0530

    worked on js concepts

---
+++ basic.js
@@ -0,0 +1,10 @@
+let message = "Hello, World!";
+const PI = 3.14;
+
+// Function
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+// Output
+console.log(greet("pradeep")); // Hello, Alice!
